344aa0dd646e8d6b16f7b93b1d027ab23c9dae95
Julia Yakovenko <julia4kosenko@gmail.com>
2021-10-30 21:03:40 +0300

    TODO-4 Added ability to mark todo as completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ClearIcon from '@mui/icons-material/Clear';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
@@ -15,6 +16,7 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import InputAdornment from '@mui/material/InputAdornment';
+import Checkbox from '@mui/material/Checkbox';
 
 import './App.css';
 
@@ -28,39 +30,54 @@ function App() {
     event.preventDefault();
 
     if (todoTitle === '') {
-      return
+      return;
     }
 
-    setTodoList((prevState) => ([
+    setTodoList((prevState) => [
       ...prevState,
       {
         title: todoTitle,
         isCompleted: false,
         id: uuidv4(),
-      }
-    ]))
+      },
+    ]);
 
     clearTitle();
   }
 
   function onTitleChange(event) {
-    setTodoTitle(event.target.value)
+    setTodoTitle(event.target.value);
   }
 
   function clearTitle() {
-    setTodoTitle('')
+    setTodoTitle('');
   }
 
   function deleteTodoItem(todoId) {
     return () => {
       const filteredTodoList = todoList.filter((item) => {
-        return item.id !== todoId
+        return item.id !== todoId;
       });
 
-      setTodoList(filteredTodoList)
-    }
+      setTodoList(filteredTodoList);
+    };
   }
 
+  function changeIsCompleted(todoId) {
+    return () => {
+      const newTodoList = todoList.map((item) => {
+        if (item.id === todoId) {
+          return {
+            ...item,
+            isCompleted: !item.isCompleted,
+          };
+        }
+        return item;
+      });
+
+      setTodoList(newTodoList);
+    };
+  }
 
   return (
     <Container maxWidth="sm" sx={{ pt: 2 }}>
@@ -86,30 +103,43 @@ function App() {
               />
             </Grid>
             <Grid item>
-              <Button type="submit" variant="contained">Add</Button>
+              <Button type="submit" variant="contained">
+                Add
+              </Button>
             </Grid>
           </Grid>
         </form>
       </Box>
-      <Paper>
-
-      <List>
-        {todoList.map((item) => {
-          return (
-            <ListItem
-              key={item.id}
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete" type="button" onClick={deleteTodoItem(item.id)}>
-                  <DeleteIcon />
-                </IconButton>
-              }
-            >
-              <ListItemText primary={item.title} />
-            </ListItem>
-          )
-        })}
-      </List>
-      </Paper>
+      {todoList.length > 0 ? (
+        <Paper>
+          <List>
+            {todoList.map((item) => {
+              return (
+                <ListItem
+                  key={item.id}
+                  pri
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete" type="button" onClick={deleteTodoItem(item.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={item.isCompleted}
+                      onChange={changeIsCompleted(item.id)}
+                      inputProps={{ 'aria-label': 'Mark todo as completed' }}
+                      title="Mark todo as completed"
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary={item.title} className={item.isCompleted ? 'todoIsCompeted' : ''} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </Paper>
+      ) : null}
     </Container>
   );
 }
