8a3384cc8a728f98736a8a2ce38a72c343ac0e7f
Julia Yakovenko <julia4kosenko@gmail.com>
2022-10-30 18:07:45 +0200

    TODO-5 Updated npm dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const rootNode = document.getElementById('root')
+const root = createRoot(rootNode)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
